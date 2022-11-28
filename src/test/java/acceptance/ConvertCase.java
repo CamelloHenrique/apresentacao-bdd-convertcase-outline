c56b051a5156f6b42718f34ed28c0f2835f6ecc0
CamelloHenrique <hc.camello@gmail.com>
2022-11-28 17:51:15 -0300

    Correção no nome da classe ConvertCase

--- src/test/java/acceptance/ConvertCase.java
+++ src/test/java/acceptance/ConvertCase.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Convertcase {
+public class ConvertCase {
 	private WebDriver driver = null;
 
 	@Before
